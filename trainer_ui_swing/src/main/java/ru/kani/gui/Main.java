0acf38404053773fc271325f2f2ecde3e89333fa
kanidzham <dannakim83@gmail.com>
2025-03-28 21:42:53 +0300

    ДЗ 7

--- trainer_ui_swing/src/main/java/ru/kani/gui/Main.java
+++ trainer_ui_swing/src/main/java/ru/kani/gui/Main.java
@@ -1,17 +1,24 @@
 package ru.kani.gui;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.kani.domain.service.QuestionService;
 import ru.kani.gui.config.SpringConfig;
 import ru.kani.gui.controller.MainController;
 
+
 import javax.swing.*;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Начало работы приложения");
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         QuestionService questionService = context.getBean(QuestionService.class);
         SwingUtilities.invokeLater(new MainController(questionService));
+        logger.info("Конец работы приложения");
     }
 }
--- trainer_ui_swing/src/main/java/ru/kani/gui/config/SpringConfig.java
+++ trainer_ui_swing/src/main/java/ru/kani/gui/config/SpringConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import ru.kani.spring.jdbc.config.DbConfig;
+import ru.kani.spring.hibernate.config.DbConfig;
+
 
 @Configuration
 @Import(DbConfig.class)
