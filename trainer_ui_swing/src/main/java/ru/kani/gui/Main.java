7ec5828a6ae9b7a37062eacf1afe7c1c95b1f4e5
kanidzham <dannakim83@gmail.com>
2025-03-28 22:05:36 +0300

    ДЗ 8

--- trainer_ui_swing/src/main/java/ru/kani/gui/Main.java
+++ trainer_ui_swing/src/main/java/ru/kani/gui/Main.java
@@ -2,7 +2,10 @@ package ru.kani.gui;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.kani.domain.service.QuestionService;
 import ru.kani.gui.config.SpringConfig;
@@ -11,13 +14,16 @@ import ru.kani.gui.controller.MainController;
 
 import javax.swing.*;
 
-public class Main {
+
+@SpringBootApplication
+public class Main  {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Main.class)
+                .headless(false).run(args);
         logger.info("Начало работы приложения");
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-        QuestionService questionService = context.getBean(QuestionService.class);
+        QuestionService questionService = ctx.getBean(QuestionService.class);
         SwingUtilities.invokeLater(new MainController(questionService));
         logger.info("Конец работы приложения");
     }
